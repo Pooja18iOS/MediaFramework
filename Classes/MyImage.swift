381ed3fb1134753fb5ee8d1a0b9ab2f4f7963e69
Pooja18iOS <pooja.trivedi@theonetechnologies.co.in>
2023-10-30 12:22:50 +0530

    - added another methods to get assets

--- Classes/MyImage.swift
+++ Classes/MyImage.swift
@@ -8,8 +8,30 @@
 import Foundation
 
 public class MyImage {
-  public class func getImage() -> UIImage? {
-      let bundle = Bundle(for: self)
-      return UIImage(named: "Ocean", in: bundle, compatibleWith: nil)
-  }
+    public class func getImage() -> UIImage? {
+        let bundle = Bundle(for: self)
+        return UIImage(named: "Ocean", in: bundle, compatibleWith: nil)
+    }
+    
+    public class ImageProvider {
+        // convenient for specific image
+        public static func picture() -> UIImage {
+            if #available(iOS 13.0, *) {
+                return UIImage(named: "Ocean", in: Bundle(for: self), with: nil) ?? UIImage()
+            } else {
+                // Fallback on earlier versions
+                return UIImage()
+            }
+        }
+        
+        // for any image located in bundle where this class has built
+        public static func image(named: String) -> UIImage? {
+            if #available(iOS 13.0, *) {
+                return UIImage(named: named, in: Bundle(for: self), with: nil)
+            } else {
+                // Fallback on earlier versions
+                return UIImage()
+            }
+        }
+    }
 }
